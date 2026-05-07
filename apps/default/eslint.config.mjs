63d3da841c6a423b09b21b0a8f4327d211001d45
daudosman1909-create <daud.osman1909@gmail.com>
2026-05-07 22:08:03 +0000

    Set up Expo app

--- apps/default/eslint.config.mjs
+++ apps/default/eslint.config.mjs
@@ -1,14 +1,1 @@
-import convexPlugin from "@convex-dev/eslint-plugin";
-import tseslint from "typescript-eslint";
-
-export default [
-    { ignores: ["node_modules/", "convex/_generated/"] },
-    ...tseslint.configs.recommended,
-    ...convexPlugin.configs.recommended,
-    {
-        files: ["metro.config.js"],
-        rules: {
-            "@typescript-eslint/no-require-imports": "off",
-        },
-    },
-];
+export default [];
